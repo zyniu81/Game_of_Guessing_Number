85ffdfe036b9e88e7063d3f95ce15b42f4bcc946
zyniu81@wp.pl <zyniu81>
2024-01-18 15:35:58 +0100

    Second verssion

--- guessing_number.py
+++ guessing_number.py
@@ -2,25 +2,28 @@ import random
 
 
 def guessing_number():
-    computer_number = random.randint(1, 100)
-    player_number = int(input("Guess the number between 1 and 100: "))
-    try:
-        if player_number < 1 or player_number > 100:
-            print("Please enter a number between 1 and 100")
-        elif player_number < computer_number:
-            print("Too small!")
-        elif player_number > computer_number:
-            print("Too big!")
-        elif player_number == computer_number:
-            print("You win!")
-        else:
+    while True:
+        try:
+            computer_number = random.randint(1, 100)
+            player_number = int(input("Guess the number between 1 and 100: "))
+            if player_number < 1 or player_number > 100:
+                print("Please enter a number between 1 and 100")
+            elif player_number < computer_number:
+                print("Too small!")
+            elif player_number > computer_number:
+                print("Too big!")
+            elif player_number == computer_number:
+                print("You win!")
+                break
+            else:
+                print("It's not a number!")
+            return guessing_number()
+        except ValueError:
             print("It's not a number!")
-        return guessing_number()
-    except ValueError:
-        print("It's not a number!")
-        return guessing_number()
-    except KeyboardInterrupt:
-        print("Interrupted by the player")
+            return guessing_number()
+        except KeyboardInterrupt:
+            print("Interrupted by the player")
+            break
 
 
-print(guessing_number())
+(guessing_number())
